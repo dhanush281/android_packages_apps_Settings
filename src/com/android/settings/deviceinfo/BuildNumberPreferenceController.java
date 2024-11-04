9dc306018097f5ea4c593d22cc1e1d11d3faa766
clarencelol <clarencekuiek@proton.me>
2024-11-04 13:24:03 +0800

    Revert "Settings: Append custom version on build display id"
    * Already exists in Pixelage version
    
    This reverts commit 59906bd61039e9ef8e0c769f4f26f33b085ef352.

--- src/com/android/settings/deviceinfo/BuildNumberPreferenceController.java
+++ src/com/android/settings/deviceinfo/BuildNumberPreferenceController.java
@@ -80,14 +80,7 @@ public class BuildNumberPreferenceController extends BasePreferenceController im
 
     @Override
     public CharSequence getSummary() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY));
-        String PixelageVersion = VersionUtils.getPixelageVersion();
-        if (!PixelageVersion.equals("")){
-            sb.append("\n");
-            sb.append(PixelageVersion);
-        }
-        return sb.toString();
+        return BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY);
     }
 
     @Override
